eb14ea89f90723086af842da2fde5a113b8f6b69
alangtzdev <alan_isgure@hotmail.com>
2019-01-29 14:55:32 -0600

    Messenger Ok

--- index.php
+++ index.php
@@ -1134,24 +1134,40 @@
   <script src="js/components/maginific-popup.min.js" type="text/javascript"></script>
   <script src="js/components/progress-bar.min.js" type="text/javascript"></script>
   <script src="js/components/masonry.min.js" type="text/javascript"></script>
+  <div>
+  <!-- https://www.youtube.com/watch?v=jdiiAlecXG4 -->
+    <!-- Load Facebook SDK for JavaScript -->
+  <div id="fb-root"></div>
+  
+  <script>
+  window.fbAsyncInit = function(){
+    FB.init({
+      appID : '2015926735143265',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v3.2'
+    });
+  };
+  (function(d, s, id) {
+   var js, fjs = d.getElementsByTagName(s)[0];
+   console.log(fjs);
+   if (d.getElementById(id)) return;
+   js = d.createElement(s); js.id = id;
+   js.src = 'https://connect.facebook.net/es_ES/sdk/xfbml.customerchat.js';
+   fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
+      
+
+  <!-- Your customer chat code -->
+  <div class="fb-customerchat"
+   attribution=setup_tool
+   page_id="396313837481341"
+   theme_color="#7ad20d"
+   logged_in_greeting="Hola!  Podemos ayudarte en algo?"
+   logged_out_greeting="Hola!  Podemos ayudarte en algo?">
+  </div>
+  </div>
 
 </body>
-<!-- Load Facebook SDK for JavaScript -->
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
 
-<!-- Your customer chat code -->
-<div class="fb-customerchat"
-  attribution=setup_tool
-  page_id="396313837481341"
-  theme_color="#7ad20d"
-  logged_in_greeting="Hola! Como puedo ayudarte?"
-  logged_out_greeting="Hola! Como puedo ayudarte?">
-</div>
 </html>
