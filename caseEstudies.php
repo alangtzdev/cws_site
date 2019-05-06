5de5398a73506d690bec82c8804c8527b8e63c12
alangtzdev <alan_isgure@hotmail.com>
2019-05-06 14:56:09 -0500

    ok para la nueva version

---
+++ caseEstudies.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Covenant Software | Paginas web | Aplicaciones moviles | Aplicaciones web | Desarrollo | Covenant Software</title>
+  <meta http-equiv="X-UA-Compatible" name="" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta property="og:locale" content="es_ES">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="Desarrollo de aplicaciones web, móviles y paginas web | Covenant Software">
+  <meta property="og:url" content="https://www.covenantsw.com/">
+  <meta property="og:site_name" content="Covenant Software">
+  <meta property="article:author" content="http://www.facebook.com/covenantsoftware">
+  <meta name="robots" content="index,follow" />
+  <meta property="og:description" content="Desarrollo de aplicaciones web, móviles y paginas web | Las 24/7 los 365 dias del año. Aprovecha nuestra promocion de actualizacion, hazlo bien, hazlo con nosotros crea o renueva tu pagina web. | Desarrollo Web">
+  <meta name="google-site-verification" content="iMmNn_8bD7X6z4cOqZSRjaa3hKGJr3eXCThcG9TgETw" />
+  <link rel="canonical" href="https://www.covenantsw.com">
+  <!-- Master Style -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700" rel="stylesheet">
+  <link rel="stylesheet" href="plugins/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap4.min.css">
+  <!-- recurso adicionales  -->
+  <link href="css/animate.css" rel="stylesheet">
+  <link href="plugins/swiper/css/swiper.min.css" rel="stylesheet" type="text/css" />
+  <link href="plugins/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css" />
+  <link href="plugins/waitme/waitMe.css" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" href="plugins/sweetalert.css">
+  <link rel="alternate" hreflang="es-mx" href="http://www.covenantsw.com/" />
+
+  <!-- estili personalizado -->
+  <link rel="stylesheet" href="css/caseStyle.css">
+  <link rel="stylesheet" href="css/caseResponsive.css">
+  <!-- ========== FAVICON & APPLE ICONS ========== -->
+  <link rel="shortcut icon" href="img/favicon.png">
+  <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+
+</head>
+
+<body id="body" data-spy="scroll" data-target=".header">
+
+<header id="header" class="default">
+        <div class="topbar">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="topbar-left  text-center text-md-left">
+                            <ul class="list-inline">
+                                <li> <i class="fa fa-map-marker"> </i> Cdlla. del Márquez 845-F, SLP</li>
+                                <li> <i class="fa fa-headphones"></i> +52 444 1240916</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="topbar-right text-center text-md-right">
+                            <ul class="list-inline">
+                                <li><a href="#"> Volver al menu principal </a> </li>
+                                <li><a href="#"> Enviarnos un mensaje</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>   
+</header>
+
+    
+<section class="intro-title black-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="intro-content">
+                        <div class="intro-name">
+                            <h3 class="text-white">Case Studie</h3>
+                            <ul class="breadcrumb mt-1">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item active">Case Studie</li>
+                            </ul>
+                        </div>
+                        <div class="intro-img">
+                            <img class="img-fluid" src="images/breadcrumb/02.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
+<section class="case-studies single page-section-ptb">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="single-image">
+                        <img class="img-fluid" src="images/case-studies/01.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="single-detail">
+                        <h4 class="mb-2">case studies single</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti amet ipsum harum natus dicta nobis facere, aliquid necessitatibus error itaque dignissimos magnam quis atque accusamus, minus. Ab obcaecati, dolore maxime.</p>
+                        <div class="single-info mt-2">
+                            <h6>clients</h6>
+                            <p class="mb-2">SOEhub lorem ipsum dolo </p>
+                            <h6>Date</h6>
+                            <p class="mb-2">20 may 2017 </p>
+                            <h6>Project Type</h6>
+                            <p class="mb-2">SOEhub </p>
+                            <a class="button border" href="#">lauch site </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr class="my-5">
+            <div class="single-banner">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="banner-block">
+                            <div class="banner-image">
+                                <div class="banner-info">
+                                    <h4 class="mb-3">CHALLENGE & SOLUTION </h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid iure cum quasi. Voluptatibus eum iure accusamus, eius dolorum reiciendis facilis molestias minus quia nemo aut, magni asperiores quas, odit adipisci Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea deserunt magni, temporibus labore dolorum consectetur hic nam alias suscipit voluptate. Eveniet aspernatur placeat recusandae commodi iusto sint nisi ipsum eaque!?</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid iure cum quasi. Ea deserunt magni, temporibus labore dolorum consectetur hic nam alias suscipit voluptate. Eveniet aspernatur placeat recusandae commodi iusto sint nisi ipsum eaque!?</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <img class="img-fluid" src="images/device/17.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="single-navigation mt-4">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="navigation-next">
+                            <a href="#">
+                                <img class="img-fluid" src="images/case-studies/01.jpg" alt="">
+                                <div class="port-arrow">
+                                    <i class="fa fa-angle-left"></i>
+                                </div>
+                                <span>CASE STUDIES SINGLE</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="navigation-previous text-right">
+                            <a href="#">
+                                <img class="img-fluid" src="images/case-studies/02.jpg" alt="">
+                                <div class="port-arrow">
+                                    <i class="fa fa-angle-right"></i>
+                                </div>
+                                <span>CASE STUDIES SINGLE</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
+<section class="case-studies single page-section-ptb">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="single-image">
+                        <img class="img-fluid" src="images/case-studies/01.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="single-detail">
+                        <h4 class="mb-2">case studies single</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti amet ipsum harum natus dicta nobis facere, aliquid necessitatibus error itaque dignissimos magnam quis atque accusamus, minus. Ab obcaecati, dolore maxime.</p>
+                        <div class="single-info mt-2">
+                            <h6>clients</h6>
+                            <p class="mb-2">SOEhub lorem ipsum dolo </p>
+                            <h6>Date</h6>
+                            <p class="mb-2">20 may 2017 </p>
+                            <h6>Project Type</h6>
+                            <p class="mb-2">SOEhub </p>
+                            <a class="button border" href="#">lauch site </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr class="my-5">
+            <div class="single-banner">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="banner-block">
+                            <div class="banner-image">
+                                <div class="banner-info">
+                                    <h4 class="mb-3">CHALLENGE & SOLUTION </h4>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid iure cum quasi. Voluptatibus eum iure accusamus, eius dolorum reiciendis facilis molestias minus quia nemo aut, magni asperiores quas, odit adipisci Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea deserunt magni, temporibus labore dolorum consectetur hic nam alias suscipit voluptate. Eveniet aspernatur placeat recusandae commodi iusto sint nisi ipsum eaque!?</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid iure cum quasi. Ea deserunt magni, temporibus labore dolorum consectetur hic nam alias suscipit voluptate. Eveniet aspernatur placeat recusandae commodi iusto sint nisi ipsum eaque!?</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <img class="img-fluid" src="images/device/17.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="single-navigation mt-4">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="navigation-next">
+                            <a href="#">
+                                <img class="img-fluid" src="images/case-studies/01.jpg" alt="">
+                                <div class="port-arrow">
+                                    <i class="fa fa-angle-left"></i>
+                                </div>
+                                <span>CASE STUDIES SINGLE</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="navigation-previous text-right">
+                            <a href="#">
+                                <img class="img-fluid" src="images/case-studies/02.jpg" alt="">
+                                <div class="port-arrow">
+                                    <i class="fa fa-angle-right"></i>
+                                </div>
+                                <span>CASE STUDIES SINGLE</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
+
+      <!--Core plugins  -->
+  <script src="plugins/jquery.min.js" charset="utf-8"></script>
+  <script src="plugins/jquery-migrate.min.js" type="text/javascript"></script>
+  <script src="plugins/poppers.js" charset="utf-8"></script>
+  <script src="plugins/bootstrap/js/bootstrap4.min.js" charset="utf-8"></script>
+  <!-- additional plugins -->
+  <script src="plugins/jquery.pulsate.min.js"></script>
+  <script src="plugins/typed.min.js"></script>
+  <!-- <script src="plugins/gs/TweenMax.min.js" charset="utf-8"></script>
+  <script src="plugins/gs/TimelineMax.min.js" charset="utf-8"></script>
+  <script src="plugins/gs/SplitText.min.js" charset="utf-8"></script> -->
+  <script src="plugins/jquery.easing.js" type="text/javascript"></script>
+  <script src="plugins/jquery.back-to-top.js" type="text/javascript"></script>
+  <script src="plugins/jquery.smooth-scroll.js" type="text/javascript"></script>
+  <script src="plugins/jquery.wow.min.js" type="text/javascript"></script>
+  <script src="plugins/swiper/js/swiper.jquery.min.js" type="text/javascript"></script>
+  <script src="plugins/magnific-popup/jquery.magnific-popup.min.js" type="text/javascript"></script>
+  <script src="plugins/jquery.appear.js" type="text/javascript"></script>
+  <script src="plugins/masonry/jquery.masonry.pkgd.min.js" type="text/javascript"></script>
+  <script src="plugins/masonry/imagesloaded.pkgd.min.js" type="text/javascript"></script>
+  <script src="plugins/jquery.mask.js" charset="utf-8"></script>
+  <!-- <script src="plugins/jquery.ajaxchimp.min.js"></script> -->
+  <script src="plugins/jquery.validate.min.js"></script>
+  <script src="plugins/waitme/waitMe.js"></script>
+  <script src="plugins/sweetalert-dev.js"></script>
+  <script src="js/components/messages_es.js"></script>
+  <script src="js/components/gmap.js" type="text/javascript"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyChlWep1CsPxzOXk6ZS-4HMJdQqOCBKQdA&amp;callback=initMap"></script>
+  <!-- PAGE LEVEL SCRIPTS -->
+  <script src="js/layout.js"></script>
+  <script src="js/index.js" charset="utf-8"></script>
+  <script src="js/components/wow.min.js" type="text/javascript"></script>
+  <script src="js/components/swiper.min.js" type="text/javascript"></script>
+  <script src="js/components/maginific-popup.min.js" type="text/javascript"></script>
+  <script src="js/components/progress-bar.min.js" type="text/javascript"></script>
+  <script src="js/components/masonry.min.js" type="text/javascript"></script>
+  <script src="plugins/countdowntime/moment.min.js"></script>
+  <script src="plugins/countdowntime/moment-timezone.min.js"></script>
+ <script src="plugins/countdowntime/moment-timezone-with-data.min.js"></script>
+  <script src="plugins/countdowntime/countdowntime.js"></script>
+
+</body>
+
+</html>
